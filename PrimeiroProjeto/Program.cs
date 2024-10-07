0dcf1140b0a2162e90b3023893b161dda22dafc7
giovaniCheloni <130862039+giovaniCheloni@users.noreply.github.com>
2024-10-07 16:40:04 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite sua senha: ");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while (x != 2002)
+            {
+                Console.WriteLine("Senha Invalida!");                
+                Console.Write("Digite sua senha: ");
+                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+
+}
